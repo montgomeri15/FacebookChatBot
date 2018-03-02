9747a935017ec67fe500647d0711404d824a52e9
montgomeri15 <montgomeri15@gmail.com>
2018-03-02 22:04:46 +0200

    Personalized page.

--- FlaskTest.py
+++ FlaskTest.py
@@ -2,9 +2,9 @@ from flask import Flask
 app = Flask(__name__)  # name - аргумент для определения пути к корневому каталогу
 
 
-@app.route('/user')
-def index():  # функция представления
-    return '<h1>Hello World!</h1>'
+@app.route('/user/<name>')
+def index(name):  # функция представления
+    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':  # сервер запускается только при выполнении сценария
