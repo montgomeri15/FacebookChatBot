8b3f13cc8065dc132d7d235f1881bf56fd339a57
montgomeri15 <montgomeri15@gmail.com>
2018-03-05 00:20:49 +0200

    Ready to register on facebook.

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request  # Импортируем библиотеку Flask
+from pymessenger import Bot  # Импортируем нужный PyMessenger с ботом (https://github.com/davidchua/pymessenger)
+import random
 
 app = Flask(__name__)  # Создаем объект приложения (наследуя Flask)
 
+ACCESS_TOKEN = 'YOUR_ACCESS_TOKEN'
+VERIFY_TOKEN = 'YOUR_VERIFY_TOKEN'
 
-@app.route('/', methods=['GET', 'POST'])  # Выведет в http://localhost:5000/
-def hello():
-    return 'Hello World!'
+bot = Bot(ACCESS_TOKEN)
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/', methods=['GET', 'POST'])  # Выведет в http://localhost:5000/. Получаем здесь соо от ФБ боту.
+def receive_message():
 
     # до того как позволить людям отправлять что-либо боту, Facebook проверяет маркер,
     # подтверждающий, что все запросы, получаемые ботом, приходят из Facebook:
@@ -18,8 +20,8 @@ if __name__ == "__main__":
         token_sent = request.args["hub.verify_token"]
         return verify_fb_token(token_sent)
 
-    # если запрос не был GET, это был POST-запрос, и мы обрабатываем запрос пользователя
-    else:
+    else:  # если запрос не был GET, это был POST-запрос, и мы обрабатываем запрос пользователя
+
         # получаем сообщение, отправленное пользователем боту
         output = request.get_json()
         for event in output['entry']:
@@ -33,18 +35,25 @@ if __name__ == "__main__":
                     if message['message'].get('attachments'):  # если это вложение (не текст), то:
                         response_sent_nontext = get_message()
                         send_message(recipient_id, response_sent_nontext)
-        return 'Message Processed'
+        return 'Сообщение обработано.'
+
 
 def verify_fb_token(token_sent):  # функция проверки токена/маркера (бот должен его иметь для ФБ)
-    '''Сверяет токен, отправленный фейсбуком, с имеющимся у вас.
-        При соответствии позволяет осуществить запрос, в обратном случае выдает ошибку.'''
-    if token_sent == VERIFY_TOKEN:
-        return request.args.get("hub.challenge")
-    return 'Invalid verification token'
+    if token_sent == VERIFY_TOKEN:  # Сверяет токен ФБ и наш. При совпадении можно делать запрос, иначе - ошибка.
+        return request.args.get('hub.challenge')
+    return 'Ошибка: неверный токен.'
+
 
 def send_message(recipient_id, response):
-    '''Отправляет пользователю текстовое сообщение в соответствии с параметром response.'''
-    bot.send_text_message(recipient_id, response)
-    return "success"
+    bot.send_text_message(recipient_id, response)  # отправляет пользователю текст в соответствии с параметром response
+    return 'Успешно!'
 
 
+def get_message():
+    sample_responses = ['Потрясающе!', 'Я вами горжусь!', 'Продолжайте в том же духе!',
+                        'Лучшее, что я когда-либо видел!']  # отправляет случайные сообщения юзеру
+    return random.choice(sample_responses)
+
+
+if __name__ == "__main__":
+    app.run()
